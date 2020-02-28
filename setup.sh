55223df592bacc3c5211081bb06aa24c3eb11c85
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-02-28 09:25:29 +0000

    Fix bash script?

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # Fjern upstream branch
 git remote rm upstream
 # Legg til upstream til fagstab-repoet på alle gitpod instanser som blir kjørt av studenter
-git remote add upstream git://github.com/jorgensta/gitpod-test.git
+git remote add upstream github.com/jorgensta/gitpod-test.git
