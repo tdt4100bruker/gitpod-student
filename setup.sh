67bc1ff44091e032ba7d670bbbc891b41233383d
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-03-01 12:54:41 +0000

    Got remote gitpod-student repo available

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # Removes any upstream branch
 #
 git remote rm upstream
+
 #
 # Adding upstream branch for lecturer/teacher main repository.
 # works if and only if a correct maintainer opens the repository
