47c506d34a584f1b08dd176f337238a49f469ade
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-03-01 10:46:39 +0000

    More detailed description of setup.sh for upstream branches

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 #
 git remote rm upstream
 #
-# Adding upstream branch for current lecturer repository.
+# Adding upstream branch for lecturer/teacher main repository.
+# works if and only if a correct maintainer opens the repository
+# (github organizations, team-members)  
 #
 git remote add upstream github.com/jorgensta/gitpod-test.git
