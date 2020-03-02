c1d46a1edd5ff35f996067da4cf6e6ec8c965406
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-03-02 17:35:28 +0000

    Rename README.md for øving1

--- command.sh
+++ command.sh
@@ -6,6 +6,6 @@ git remote rm upstream
 #
 # Adding upstream branch for lecturer/teacher main repository.
 # works if and only if a correct maintainer opens the repository
-# (github organizations, team-members)  
+# (github organizations, team-members)
 #
 git remote add upstream https://github.com/jorgensta/gitpod-test.git
